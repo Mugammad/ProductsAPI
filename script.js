822eef274cb90ba92c402b0fa57a0a6e7490e0fc
Mugammad <you@example.commugammadbreda1999@gmail.com>
2022-01-04 16:08:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+fetch("https://fakestoreapi.com/products")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        data.forEach(element => {
+            document.getElementById('output').innerHTML += `
+                    <div class="col-md-3 border border-primary box p-2 m-2">
+                        <img src="${element.image}" class="img-fluid">
+                        <h4 class="pt-3">${element.title}</h4>
+                        <h5>R ${element.price}</h6>
+                        <p>${element.description}</p>
+                    </div>
+            `
+        });
+    });
